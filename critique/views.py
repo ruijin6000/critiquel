3fb4407ca1a3f38b30acbe8173906118afd5c6f1
Dev Kapupara <devkapupara@gmail.com>
2019-04-20 00:48:05 -0700

    UI refining and added details to webpages

--- critique/views.py
+++ critique/views.py
@@ -6,6 +6,36 @@ states_list = {'Alabama': 'AL', 'Alaska': 'AK', 'American Samoa': 'AS', 'Arizona
 
 mongo = PyMongo(app, uri="mongodb://ec2-54-241-147-8.us-west-1.compute.amazonaws.com:27022/yelpData")
 
+@app.route('/business', methods=['POST', 'GET'])
+def business():
+
+    if request.method == 'POST':
+        parameters = {}
+        name = capitalize_each_word(request.form['name'])
+        if len(name) > 0:
+            parameters['name'] = name
+
+        city = capitalize_each_word(request.form['city'])
+        if len(city) > 0:
+            parameters['city'] = city
+
+        state_input = request.form['state']
+        if len(state_input) > 0:
+            state = state_input.upper() if len(state_input) == 2 else states_list[state_input.capitalize()]
+            parameters['state'] = state
+
+        ratings = request.form['ratings']
+        if len(ratings) > 0:
+            ratings = int(ratings)
+            parameters['stars'] = {'$gte': ratings}
+
+        projection = {'name': 1, 'city': 1, 'address': 1, 'categories': 1, 'stars': 1}
+
+        result = mongo.db.business.find(parameters, projection).limit(20)
+        return render_template('business.html', data=result)
+
+    return render_template('business.html')
+
 def capitalize_each_word(word):
     return ' '.join(i.capitalize() for i in word.split(" "))
 
@@ -17,53 +47,12 @@ def index():
 def credits():
     return render_template('credits.html')
 
-@app.route('/business', methods=['POST', 'GET'])
-def business():
-    # Query field will never be empty since input is mandatory
-    # If user inputted data in the form, we would have a post request.
-    if request.method == 'POST':
-        search_dictionary = {}
-        # Get the input using the form fields and capitalize each word
-        name = capitalize_each_word(request.form['name'])
-        if len(name) > 0:
-            search_dictionary['name'] = name
-
-        city = capitalize_each_word(request.form['city'])
-        if len(city) > 0:
-            search_dictionary['city'] = city
-
-        state_input = request.form['state']
-        if len(state_input) > 0:
-            state = state_input.upper() if len(state_input) == 2 else states_list[state_input.capitalize()]
-            search_dictionary['state'] = state
-
-        ratings = int(request.form['ratings'])
-        print(ratings)
-        search_dictionary['stars'] = ratings
-        # Search parameters
-
-        # Projection parameter
-        projection = {'name': 1, 'city': 1, 'address': 1, 'categories': 1, 'stars': 1}
-
-        # Get the query result and pass it to render the template
-        print(f'db.business.find({search_dictionary},{projection})')
-        result = mongo.db.business.find(search_dictionary, projection).limit(20)
-        return render_template('business.html', data=result)
-    # Else we have a get request, so don't pass data (will be None by default)
-    return render_template('business.html')
-    result = mongo.db.business.find({'state':'CA'}, {'name':1,'address':1,'categories':1})
-    # result = mongo.db.business.find({'city':'Phoenix'}, {'name':1,'address':1,'categories':1})
-    return render_template('business.html', data=result)
-
-
 @app.route('/businessByCity')
 def businessByCity():
-	print()
 	result = mongo.db.business.find({'city':'Phoenix'}, {'name':1,'address':1,'categories':1})
 	return render_template('businessByCity.html', data=result)
 
 @app.route('/businessByName')
 def businessByName():
-	print()
 	result = mongo.db.business.find({'name':'Arizona Biltmore Golf Club'}, {'name':1,'address':1,'city':1, 'state':1})
 	return render_template('businessByCity.html', data=result)
