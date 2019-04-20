669de327446d2728e0f8ffd12f9b933ad501be7d
Dev Kapupara <devkapupara@gmail.com>
2019-04-19 17:28:14 -0700

    Made the database connection and added a query from the business data set

--- critique/views.py
+++ critique/views.py
@@ -2,7 +2,7 @@ from flask import render_template, request
 from flask_pymongo import PyMongo
 from . import app
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app, uri="mongodb://ec2-54-241-147-8.us-west-1.compute.amazonaws.com:27022/yelpData")
 
 @app.route('/')
 def index():
@@ -11,3 +11,9 @@ def index():
 @app.route('/credits')
 def credits():
     return render_template('credits.html')
+
+@app.route('/business')
+def business():
+    print()
+    result = mongo.db.business.find({'state':'CA'}, {'name':1,'address':1,'categories':1})
+    return render_template('business.html', data=result)
