af3734e771e0e405fe08badadd84e28ed1750acb
Neha Atul Bhagwat <nehabhagwat@Nehas-MacBook-Pro.local>
2019-04-19 19:06:24 -0700

    Added 2 functions for business by city and name

--- critique/views.py
+++ critique/views.py
@@ -16,4 +16,18 @@ def credits():
 def business():
     print()
     result = mongo.db.business.find({'state':'CA'}, {'name':1,'address':1,'categories':1})
+    # result = mongo.db.business.find({'city':'Phoenix'}, {'name':1,'address':1,'categories':1})
     return render_template('business.html', data=result)
+
+
+@app.route('/businessByCity')
+def businessByCity():
+	print()
+	result = mongo.db.business.find({'city':'Phoenix'}, {'name':1,'address':1,'categories':1})
+	return render_template('businessByCity.html', data=result)
+
+@app.route('/businessByName')
+def businessByName():
+	print()
+	result = mongo.db.business.find({'name':'Arizona Biltmore Golf Club'}, {'name':1,'address':1,'city':1, 'state':1})
+	return render_template('businessByCity.html', data=result)
