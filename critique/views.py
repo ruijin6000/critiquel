b9030b056c938e129215d36de28b863a7bc28f50
Dev Kapupara <devkapupara@gmail.com>
2019-04-20 17:51:54 -0700

    Added CRUD operations, a few UI changes to the Navigation Bar

--- critique/views.py
+++ critique/views.py
@@ -1,13 +1,14 @@
 from flask import render_template, request
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 from . import app
 
 states_list = {'Alabama': 'AL', 'Alaska': 'AK', 'American Samoa': 'AS', 'Arizona': 'AZ', 'Arkansas': 'AR', 'California': 'CA', 'Colorado': 'CO', 'Connecticut': 'CT', 'Delaware': 'DE', 'District Of Columbia': 'DC', 'Federated States Of Micronesia': 'FM', 'Florida': 'FL', 'Georgia': 'GA', 'Guam': 'GU', 'Hawaii': 'HI', 'Idaho': 'ID', 'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA', 'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA', 'Maine': 'ME', 'Marshall Islands': 'MH', 'Maryland': 'MD', 'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN', 'Mississippi': 'MS', 'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ', 'New Mexico': 'NM', 'New York': 'NY', 'North Carolina': 'NC', 'North Dakota': 'ND', 'Northern Mariana Islands': 'MP', 'Ohio': 'OH', 'Oklahoma': 'OK', 'Oregon': 'OR', 'Palau': 'PW', 'Pennsylvania': 'PA', 'Puerto Rico': 'PR', 'Rhode Island': 'RI', 'South Carolina': 'SC', 'South Dakota': 'SD', 'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT', 'Vermont': 'VT', 'Virgin Islands': 'VI', 'Virginia': 'VA', 'Washington': 'WA', 'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY'}
 
 mongo = PyMongo(app, uri="mongodb://ec2-54-241-147-8.us-west-1.compute.amazonaws.com:27022/yelpData")
 
-@app.route('/business', methods=['POST', 'GET'])
-def business():
+@app.route('/query', methods=['POST', 'GET'])
+def query():
 
     if request.method == 'POST':
         parameters = {}
@@ -32,12 +33,59 @@ def business():
         projection = {'name': 1, 'city': 1, 'address': 1, 'categories': 1, 'stars': 1}
 
         result = mongo.db.business.find(parameters, projection).limit(20)
-        return render_template('business.html', data=result)
+        return render_template('query.html', data=result)
 
-    return render_template('business.html')
+    return render_template('query.html')
 
-def capitalize_each_word(word):
-    return ' '.join(i.capitalize() for i in word.split(" "))
+@app.route('/insert', methods=['POST', 'GET'])
+def insert():
+    if request.method == 'POST':
+        name = capitalize_each_word(request.form['name'])
+        address = capitalize_each_word(request.form['address'])
+        city = capitalize_each_word(request.form['city'])
+        state_input = request.form['state']
+        if len(state_input) > 0:
+            state = state_input.upper() if len(state_input) == 2 else states_list[state_input.capitalize()]
+        category = capitalize_each_word(request.form['category'])
+        attributes = {i: True for i in capitalize_each_word(request.form['services']).split(",")}
+        ratings = capitalize_each_word(request.form['ratings'])
+        result = mongo.db.business.insert_one({'name': name, 'address': address, 'city': city,
+                                                'state': state, 'categories': category, 'attributes':attributes, 'stars': ratings})
+        return render_template('insert.html', success=True)
+    return render_template('insert.html')
+
+@app.route('/delete', methods=['POST', 'GET'])
+def delete():
+    status = 0                     # 0 means we need user input
+    if request.method == 'POST':
+        name = capitalize_each_word(request.form['name'])
+        address = capitalize_each_word(request.form['address'])
+        city = capitalize_each_word(request.form['city'])
+        state_input = request.form['state']
+        if len(state_input) > 0:
+            state = state_input.upper() if len(state_input) == 2 else states_list[state_input.capitalize()]
+
+        result = mongo.db.business.delete_one({'name': name, 'address': address, 'city':city, 'state':state})
+        status = 1 if result.deleted_count == 1 else -1
+        return render_template('delete.html', success=status)
+    return render_template('delete.html', success=status)
+
+@app.route('/update', methods=['POST', 'GET'])
+def update():
+    status = 0                     # 0 means we need user input
+    if request.method == 'POST':
+        name = capitalize_each_word(request.form['name'])
+        address = capitalize_each_word(request.form['address'])
+        city = capitalize_each_word(request.form['city'])
+        state_input = request.form['state']
+        if len(state_input) > 0:
+            state = state_input.upper() if len(state_input) == 2 else states_list[state_input.capitalize()]
+        new_rating = int(request.form['rating'])
+
+        result = mongo.db.business.update_one({'name': name, 'address': address, 'city':city, 'state':state}, {'$set': {'stars':new_rating}})
+        status = 1 if result.modified_count == 1 else -1
+        return render_template('update.html', success=status)
+    return render_template('update.html', success=status)
 
 @app.route('/')
 def index():
@@ -47,12 +95,5 @@ def index():
 def credits():
     return render_template('credits.html')
 
-@app.route('/businessByCity')
-def businessByCity():
-	result = mongo.db.business.find({'city':'Phoenix'}, {'name':1,'address':1,'categories':1})
-	return render_template('businessByCity.html', data=result)
-
-@app.route('/businessByName')
-def businessByName():
-	result = mongo.db.business.find({'name':'Arizona Biltmore Golf Club'}, {'name':1,'address':1,'city':1, 'state':1})
-	return render_template('businessByCity.html', data=result)
+def capitalize_each_word(word):
+    return ' '.join(i.capitalize() for i in word.split(" "))
